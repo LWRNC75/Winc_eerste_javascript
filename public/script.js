6c964e5bcfa58eadb341fb0def3ba493e27fffc8
LWRNC75 <lawrence.kartodimedjo@gmail.com>
2020-09-07 21:57:46 +0200

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,32 @@
+// 4.
+console.log('Hello Winc Academy');
+// 5.
+let name = 'Lawrence';
+console.log(name);
+// 6.
+// Variable met if kan niet
+// 7.
+let sum = 4 + 4;
+console.log(sum);
+// 8.
+//  Addition
+let addition = 4 + 4;
+console.log(addition);
+// Subtraction
+let subtraction = 8 - 4;
+console.log(subtraction);
+// Multiplication
+let multiplication = 8 * 4;
+console.log(multiplication);
+// Division
+let division = 8 / 4;
+console.log(division);
+// Modulus
+let modulus = 5 % 2;
+console.log(modulus);
+// 9.
+let age = 48;
+console.log(typeof age);
+// 10.
+let ageString = '48';
+console.log(typeof ageString);
